e31ac57cefb1005ad7abf8a9882120a23f1e0e52
Sachit Mahajan <sachitmahajan90@gmail.com>
2018-03-14 17:36:23 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ lassData.src = 'https://pm25.lass-net.org/GIS/IDW/data.js';
 lassData.onload = function() {
   const idw = L.idwLayer(PM25points, {
     opacity: 0.5,
-    maxZoom: 16,
+    maxZoom: 17,
     minZoom: 8,
     cellSize: 10,
     exp: 2,
